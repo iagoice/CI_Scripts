be57e064a95d7b127273f801e8eb452e3e57d6ea
Iago Mordente <iagoc@ciandt.com>
2018-07-24 18:10:30 -0300

    será

--- Jenkinsfile
+++ Jenkinsfile
@@ -4,7 +4,7 @@ pipeline {
     stages {
         stage ('build') {
             steps {
-                echo "aaaaaaaaaaaa"
+                sh('./build.sh')
             }
         }
     }
---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -l
+source iosUtils.sh
+# Go to project folder
+cd ${WORKSPACE}/Saldo de Horas/SaldoDeHoras
+# Execute fastlane command to build
+# This lane is configured on Fastfile
+# Passing Fastlane Parameters
+printf "\n\nBuilding Project...\n\n"
+buildCmd=(bundle exec fastlane build)
