ff594e090807ac511dd4f56b94aef387f613e167
Iago Mordente <iagoc@ciandt.com>
2018-07-25 11:45:00 -0300

    mudanças no jenkinsfile

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -l
-source iosUtils.sh
+# source iosUtils.sh
 # Go to project folder
 cd ../"Saldo de Horas"/SaldoDeHoras
 # Execute fastlane command to build
 # This lane is configured on Fastfile
 # Passing Fastlane Parameters
 printf "\n\nBuilding Project...\n\n"
-buildCmd=(bundle exec fastlane build)
+fastlane build
